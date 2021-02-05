69ebbfe39f7467ecb01d6c31309cac00950a7812
Pysuper <1821764535@qq.com>
2021-02-05 13:23:39 +0800

    不同的部署方式

---
+++ deploy/nginx+tornado+daphne+supervisor/TornadoServer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# __author__ = '__Jack__'
+
+import os
+import sys
+
+import tornado.httpserver
+import tornado.ioloop  # 事件循环
+import tornado.web
+import tornado.wsgi
+from django.core.wsgi import get_wsgi_application
+from tornado.options import options, define
+
+# Django Application加入查找路径中
+app_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+sys.path.append(os.path.join(app_path, 'zanhu'))  # ../zanhu/zanhu
+define("port", default=6000, type=int, help="run on the given port")
+
+
+def main():
+    tornado.options.parse_command_line()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+    wsgi_app = tornado.wsgi.WSGIContainer(get_wsgi_application())
+    http_server = tornado.httpserver.HTTPServer(wsgi_app, xheaders=True)  # xheaders=True是啥意思？
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
